78add4525fb23f87a7cf981862f12fda1c00085e
Anton <anton.zeltser@gmail.com>
2020-07-23 22:13:52 +0200

    delete duplicates from the string array

--- src/sample/MergeNames.java
+++ src/sample/MergeNames.java
@@ -6,39 +6,7 @@ public class MergeNames {
     private static String ObjectArrays;
 
 
-    public static int removeDuplicateElements(int arr[], int n){
-        if (n==0 || n==1){
-            return n;
-        }
-        int[] temp = new int[n];
-        int j = 0;
-        for (int i=0; i<n-1; i++){
-            if (arr[i] != arr[i+1]){
-                temp[j++] = arr[i];
-            }
-        }
-        temp[j++] = arr[n-1];
-        // Changing original array
-        for (int i=0; i<j; i++){
-            arr[i] = temp[i];
-        }
-        return j;
-    }
-
     public static String[] uniqueNames(String[] names1, String[] names2) {
-
-
-        int[] array1 = {1, 2, 3};
-        int[] array2 = {4, 5, 6};
-
-        int aLen = names1.length;
-        int bLen = names2.length;
-        String [] joinedArray = new String[aLen + bLen];
-
-        System.arraycopy(names1, 0, joinedArray, 0, aLen);
-        System.arraycopy(names2, 0, joinedArray, aLen, bLen);
-        System.out.println(joinedArray);
-
 
         ArrayList<String> strArrayList = new ArrayList<String>();
         for(int i=0;i<names1.length;i++)
